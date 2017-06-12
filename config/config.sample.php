2ce596d5d77cae0122642c074f2099b228897d26
Michał Tatarynowicz <tatarynowicz@gmail.com>
2017-06-12 19:54:07 +0200

    Updated config.sample.

--- config/config.sample.php
+++ config/config.sample.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 return [
     // Used as a label
     // Can also be used as a basis for a filename (i.e. ProjectAPIv1.pdf)
-    'name' => 'Test Project',
-    
+    'name'    => 'Test Project',
+
     // Where to put the generated documentation.
     // Default: 'resources/output'
     'output'  => '/full/or-relative/path',
@@ -22,17 +22,33 @@ return [
 
             // Prefix all URLs in a spec with this string.
             // Default: none
-            'prefix' => '/api/my',
+            'prefix' => '/api/foo',
 
             // Paths can be relative to raml-scoop directory or absolute.
             // You can also use PHP to figure out a path.
             // Required.
             'path'   => '../relative-to/raml-scoop-dir/specification.raml',
-            
+
             'exclude' => [
                 '/legacy-endpoint',
                 '/internal'
             ]
-        ]
+        ],
+        [
+            'name'   => 'My Git-based Spec',
+            'prefix' => '/api/bar',
+
+            // Instead of a local path you can configure a GIT repository as a source.
+            // Required.
+            'git'    => [
+                // Any GIT repository path that can be checked out
+                'uri'    => 'git@github.com:organization/repository',
+                // The name of branch to check out
+                // Default: master
+                'branch' => 'master',
+                // Path inside of the repository to take specification from.
+                'path'   => '/specs/api.raml',
+            ],
+        ],
     ]
 ];
